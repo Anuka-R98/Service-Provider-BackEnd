699f696f785518f6de879ad5cf628e6044cef607
Anuka <arrinsamurai7@gmail.com>
2023-02-22 17:06:54 +0530

    service updated

--- src/main/java/com/ServiceProvider/App/controller/ServiceController.java
+++ src/main/java/com/ServiceProvider/App/controller/ServiceController.java
@@ -2,7 +2,9 @@ package com.ServiceProvider.App.controller;
 
 import com.ServiceProvider.App.models.User;
 import com.ServiceProvider.App.payload.requests.ServiceRequest;
+import com.ServiceProvider.App.payload.requests.UserRequest;
 import com.ServiceProvider.App.payload.responses.ServiceResponse;
+import com.ServiceProvider.App.payload.responses.UserResponse;
 import com.ServiceProvider.App.repository.ServiceRepository;
 import com.ServiceProvider.App.service.ServiceService;
 
@@ -12,9 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class ServiceController {
@@ -34,6 +34,27 @@ public class ServiceController {
         } catch (Exception e) {
             log.error("Error while creating service: ", e);
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error while creating service");
+        }
+    }
+
+    /* updating service details */
+    @PutMapping("home/services/{id}")
+    @PreAuthorize("hasRole('ADMIN') or hasRole('SERVICEP')")
+    public ResponseEntity<?> updateService(@PathVariable String id, @RequestBody ServiceRequest serviceRequest){
+
+//        if (userRepository.existsByUsername(userRequest.getUsername())) {
+//            return ResponseEntity.badRequest().body("Error: Username is already taken!");
+//        }
+//        if (userRepository.existsByEmail(userRequest.getEmail())) {
+//            return ResponseEntity.badRequest().body("Error: Email is already in use!");
+//        }
+
+        try{
+            ServiceResponse serviceResponse = service.updateService(id, serviceRequest);
+            return new ResponseEntity<>(serviceResponse, HttpStatus.OK);
+        }catch(Exception e){
+            log.error("Error updating service info: ", e);
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
         }
     }
 }
--- src/main/java/com/ServiceProvider/App/service/ServiceService.java
+++ src/main/java/com/ServiceProvider/App/service/ServiceService.java
@@ -1,15 +1,17 @@
 package com.ServiceProvider.App.service;
 
+import com.ServiceProvider.App.customExceptions.ResourceNotFoundException;
 import com.ServiceProvider.App.models.MService;
 import com.ServiceProvider.App.models.User;
 import com.ServiceProvider.App.payload.requests.ServiceRequest;
 import com.ServiceProvider.App.payload.responses.ServiceResponse;
 import com.ServiceProvider.App.repository.ServiceRepository;
 
-import org.springframework.stereotype.Service;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+
 
 @Service
 public class ServiceService {
@@ -35,4 +37,21 @@ public class ServiceService {
         return serviceResponse;
     }
 
+    /* updating service details */
+    public ServiceResponse updateService(String id, ServiceRequest serviceRequest){
+        MService existingService = serviceRepository.findById(id).orElseThrow(()-> new ResourceNotFoundException("Service with id : " + id + " not found ! "));
+
+        existingService.setService_name(serviceRequest.getService_name());
+        existingService.setDescription(serviceRequest.getDescription());
+
+        log.info("Updating Service details of : " + existingService.getService_name());
+        serviceRepository.save(existingService);
+
+        ServiceResponse serviceResponse = new ServiceResponse();
+        serviceResponse.setId(existingService.getId());
+        serviceResponse.setService_name(existingService.getService_name());
+        serviceResponse.setDescription(existingService.getDescription());
+
+        return serviceResponse;
+    }
 }
